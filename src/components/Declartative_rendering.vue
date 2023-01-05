906c9303ebf6bce52ab9b816fc1bf4db2ef6205c
MerguriM <wy361307103@163.com>
2023-01-05 23:01:26 +0800

    new

---
+++ src/components/Declartative_rendering.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref } from 'vue';
+const msg = ref("hello world")
+const counter = ref({count: 0})
+</script>
+
+<template>
+    <h1>{{ msg }}</h1>
+    <p>Count is : {{ counter.count }}</p>
+</template>
