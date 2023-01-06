0aeda6965d17976d6208aec67cbf28ce21ab7796
sai <unuser@163.com>
2023-01-06 16:06:38 +0800

    3

--- src/components/Declartative_rendering.vue
+++ src/components/Declartative_rendering.vue
@@ -3,8 +3,9 @@ import { ref } from 'vue';
 const msg = ref("hello world")
 // 可以使用reactive
 const counter = ref({count: 0})
-
+// ref似乎更好用更方便，会自动包装成带.value属性的ref对象
 const state = ref(0)
+// 并且.value也是响应式的，在传递时ref也不会丢失响应性
 </script>
 
 <template>
@@ -12,6 +13,7 @@ const state = ref(0)
     <p>Count is : {{ counter.count }}</p>
     <!-- 需要直接访问counter.count的值，因为定义时声明了 -->
     <button @click="counter.count++">++</button>
+    <!-- ref对象在模板中作为顶层属性被访问时，可以自动解包成state.value -->
     <p>state is : {{ state }}</p>
     <button @click="state++">state++</button>
 </template>
