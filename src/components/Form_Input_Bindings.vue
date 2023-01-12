c4b4beef366d15013b451952fc837cb2b4cd9f84
MerguriM <wy361307103@163.com>
2023-01-13 00:15:07 +0800

    13

--- src/components/Form_Input_Bindings.vue
+++ src/components/Form_Input_Bindings.vue
@@ -9,7 +9,7 @@ const name_list = ref([])
 // 使用ref时打印和取值需要使用.value；使用reactive时则不用
 // 官方推荐我们在定义数据的时候,reactive定义复杂的数据类型的数据,ref推荐定义基本数据类型
 const new_name = ref("")
-const chosen_name = reactive([])
+const chosen_name = ref([])
 
 function push_name() {
     // 使用ref包装的时候需要使用.value，使用reactive时则不需要
@@ -35,7 +35,10 @@ function push_name() {
     <button @click="text_3 = ''">清空输入</button>
     <p>复选框绑定bool值</p>
     <label for="check"><input type="checkbox" id="check" v-model="bool_1">{{ bool_1 }}</label>
+    <br>
     <p>添加人员名单：</p>
-    <div v-for="(names, index) in name_list" :key="index"><label :for="names"><input type="checkbox" /></label></div>
+    <div v-for="(names, index) in name_list" :key="index"><label><input type="checkbox" :id="names" :value="names" v-model="chosen_name"/> {{ names }}</label></div>
     <input v-model="new_name" /><button @click="push_name">添加人员</button>
+    <p>被选召的孩子们：</p>
+    <div>{{ chosen_name }}</div>
     </template>
